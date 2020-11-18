d5a5b64afb41f714b919d995d1c45d2bea2c9ff3
AwesomeNicknameEver <katerina.muskova@gmail.com>
2020-11-18 10:27:22 +0100

    add address table

--- iis_hotel/app/Models/Hotel.php
+++ iis_hotel/app/Models/Hotel.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Hotel extends Model
-{
+class Hotel extends Model {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'oznaceni',
+        'popis',
+        'address_id'
+    ];
 }
---
+++ iis_hotel/database/migrations/2020_11_18_084510_create_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('ulice');
+            $table->string('c_popisne');
+            $table->string('mesto');
+            $table->integer('PSC');
+            $table->string('stat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
