1c3403842133ba906b4d128a37d799b824b7409e
AwesomeNicknameEver <katerina.muskova@gmail.com>
2020-11-18 12:06:53 +0100

    rearch by location

--- iis_hotel/app/Http/Controllers/HotelController.php
+++ iis_hotel/app/Http/Controllers/HotelController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Hotel;
 use Illuminate\Http\Request;
+use mysql_xdevapi\Table;
+use Illuminate\Support\Facades\DB;
 
-class HotelController extends Controller
-{
-    public function index(){
+class HotelController extends Controller {
+    public function index() {
 
     }
-    public static function get_searched($loc_pos){
-        return Hotel::where('oznaceni', 'like', "%$loc_pos%")->get();
+
+    public static function get_searched($loc_pos) {
+        $by_hotel = Hotel::where('oznaceni', 'like', "%$loc_pos%");
+
+        return DB::table('hotels')->join('addresses', function ($join) {
+            $join->on('addresses.id', '=', 'hotels.address_id');
+        })
+            ->where('addresses.mesto', 'like', "%$loc_pos%")
+            ->select('hotels.*')
+            ->union($by_hotel)
+            ->get();
     }
 }
