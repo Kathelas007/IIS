a28e55ed968e021376531b4edf825c18ebbda68b
Lord of Adminster <ttonndda@gmail.com>
2020-11-21 16:23:08 +0100

    Fixed javascript and such

--- iis_hotel/resources/views/layouts/app.blade.php
+++ iis_hotel/resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'IIS Hotel') }}</title>
 
     <!-- Scripts -->
-{{--    <script src="{{ asset('js/timepicker.js') }}" ></script>--}}
+    <script src="{{ asset('js/app.js') }}" ></script>
 
 <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
