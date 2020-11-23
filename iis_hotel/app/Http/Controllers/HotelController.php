ac09c60b59726ca9ff5744fdd26896c65ad95eaa
AwesomeNicknameEver <katerina.muskova@gmail.com>
2020-11-23 17:58:48 +0100

    search by date

--- iis_hotel/app/Http/Controllers/HotelController.php
+++ iis_hotel/app/Http/Controllers/HotelController.php
@@ -3,29 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\Hotel;
+use App\Models\Order;
 use App\Models\User;
 use App\Models\RoomType;
 use App\Models\Room;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 use Intervention\Image\ImageManagerStatic as Im;
+use function Psy\debug;
 
 class HotelController extends Controller {
 
-    protected function validator(array $data)
-    {
+    protected function validator(array $data) {
         return Validator::make($data, [
-            'oznaceni'  => ['required', 'string', 'max:255'],
-            'popis'     => ['required', 'string'],
+            'oznaceni' => ['required', 'string', 'max:255'],
+            'popis' => ['required', 'string'],
             //image
-            'street'    => ['string', 'max:255'],
-            'city'      => ['string', 'max:255'],
+            'street' => ['string', 'max:255'],
+            'city' => ['string', 'max:255'],
             'c_popisne' => ['digits_between:0,10'],
-            'PSC'       => ['digits_between:0,10']
+            'PSC' => ['digits_between:0,10']
         ]);
     }
 
@@ -34,7 +36,7 @@ class HotelController extends Controller {
     }
 
     public function index(User $user = NULL) {
-       if ( $user != NULL){
+        if ($user != NULL) {
             $hotels = Hotel::where('user_id', $user->id)->get();
         } else if (Auth::user()->isAtLeast(User::role_admin)) {
             $hotels = Hotel::All();
@@ -48,24 +50,51 @@ class HotelController extends Controller {
         return view('hotels.index', $data);
     }
 
-    public static function get_search_paginator($loc_pos) {
+    public static function get_search_paginator($loc_pos, $start, $end) {
 
-        $by_hotel = DB::table('hotels')->where('oznaceni', 'like', "%$loc_pos%")
+        $all_hotels = DB::table('hotels')
+            ->where('mesto', 'like', "%$loc_pos%")
+            ->orWhere('oznaceni', 'like', "%$loc_pos%")
             ->select('hotels.id', 'hotels.oznaceni');
 
-        return DB::table('hotels')
-            ->where('mesto', 'like', "%$loc_pos%")
-            ->select('hotels.id', 'hotels.oznaceni')
-            ->union($by_hotel)
-            ->paginate(10);
+        if ($all_hotels->get()->count() == 0) {
+            Log::info('no hotels found');
+            return $all_hotels->paginate(10);
+        }
 
-        /*return DB::table('hotels')->join('addresses', function ($join) {
-            $join->on('addresses.id', '=', 'hotels.address_id');
-        })
-            ->where('addresses.mesto', 'like', "%$loc_pos%")
-            ->select('hotels.id', 'hotels.oznaceni')
-            ->union($by_hotel)
-            ->paginate(10);*/
+        $orders = DB::table('orders')->where([['end_date', '>=', "$start"], ['start_date', '<=', "$end"]]);
+
+        if ($orders->get()->count() == 0) {
+            Log::info('no orders found in this date, return all hotels');
+            return $all_hotels->select('hotels.id AS id', 'hotels.oznaceni AS oznaceni')->paginate(10);
+        } else {
+            Log::info('found orders, filter hotels');
+        }
+
+        $orders_joined = $orders
+            ->join('room_orders', 'room_orders.order_id', '=', 'orders.id') //1
+            ->join('rooms', 'rooms.id', 'room_orders.room_id') //2
+            ->join('room_types', 'room_types.id', 'rooms.roomType_id') //3
+            ->join('hotels', 'hotels.id', '=', 'room_types.hotel_id') //4
+            ->select('hotels.id AS hotels_id', 'room_types.id AS room_type_id', 'rooms.id AS room_id');
+
+        $count = $orders_joined->get()->count();
+        echo " celkem orders: $count";
+
+
+        $hotels_orders_joined = $all_hotels->leftJoinSub($orders_joined, 'orders', function ($join) {
+            $join->on('orders.hotels_id', '=', 'hotels.id');
+        });
+
+        $filtered_hotels = $hotels_orders_joined->whereNull('orders.hotels_id');
+
+        return $filtered_hotels->paginate(10);
+
+    }
+
+    public static function get_hotel_by_oznaceni($oznaceni) {
+        return Hotel::where('oznaceni', 'like', "%$oznaceni%")
+            ->first();
     }
 
     function public_show(Request $request) {
@@ -73,7 +102,7 @@ class HotelController extends Controller {
 
         $types = RoomType::where('hotel_id', $hotel->id)->get();
         $room_types = array();
-        foreach($types as $type) {
+        foreach ($types as $type) {
             $rooms_count = Room::where('roomType_id', $type->id)->count();
             // TODO: select only available rooms
 
@@ -92,7 +121,7 @@ class HotelController extends Controller {
         return view('hotels.public_show', $data);
     }
 
-    public function owner_show(Hotel $hotel){
+    public function owner_show(Hotel $hotel) {
 
         $data = [
             'hotel' => $hotel,
@@ -101,12 +130,12 @@ class HotelController extends Controller {
         return view('hotels.owner_show', $data);
     }
 
-    public function add(){
+    public function add() {
 
         return view('hotels.add');
     }
 
-    public function store(Request $request){
+    public function store(Request $request) {
 
         $this->validator($request->all())->validate();
 
@@ -114,20 +143,20 @@ class HotelController extends Controller {
 
         $hotel = new Hotel();
         $hotel->oznaceni = $request->oznaceni;
-        $hotel->popis    = $request->popis;
-        $hotel->ulice    = $request->ulice;
+        $hotel->popis = $request->popis;
+        $hotel->ulice = $request->ulice;
         $hotel->c_popisne = $request->c_popisne;
-        $hotel->mesto     = $request->mesto;
-        $hotel->PSC       = $request->PSC;
-        $hotel->stat      = $request->stat;
+        $hotel->mesto = $request->mesto;
+        $hotel->PSC = $request->PSC;
+        $hotel->stat = $request->stat;
 
-        $hotel->user_id  = $user->id;
+        $hotel->user_id = $user->id;
         $hotel->save();
 
         return redirect('home');
     }
 
-    public function edit(Hotel $hotel){
+    public function edit(Hotel $hotel) {
 
         $data = [
             'hotel' => $hotel
@@ -135,24 +164,24 @@ class HotelController extends Controller {
         return view('hotels.edit', $data);
     }
 
-    public function update(Request $request, Hotel $hotel){
+    public function update(Request $request, Hotel $hotel) {
 
         $this->validator($request->all())->validate();
 
         $hotel->oznaceni = $request->oznaceni;
-        $hotel->popis    = $request->popis;
-        $hotel->ulice    = $request->ulice;
+        $hotel->popis = $request->popis;
+        $hotel->ulice = $request->ulice;
         $hotel->c_popisne = $request->c_popisne;
-        $hotel->mesto     = $request->mesto;
-        $hotel->PSC       = $request->PSC;
-        $hotel->stat      = $request->stat;
+        $hotel->mesto = $request->mesto;
+        $hotel->PSC = $request->PSC;
+        $hotel->stat = $request->stat;
 
         $hotel->save();
 
-            return redirect('home');
-        }
+        return redirect('home');
+    }
 
-    public function destroy ($id){
+    public function destroy($id) {
 
         $hotel = Hotel::findOrFail($id);
         $hotel->delete();
