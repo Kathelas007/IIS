ed8a1f83108ce22cc6cdd49224c874614750e93f
AwesomeNicknameEver <katerina.muskova@gmail.com>
2020-11-27 13:12:33 +0100

    add authprovider

---
+++ odevzdani/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Auth;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Blade::if('authAtLeast', function ($user_role) {
+            return Auth::user()->isAtLeast($user_role);
+        });
+    }
+}
