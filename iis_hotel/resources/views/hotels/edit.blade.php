a7a1ee10d81b904f6c12d79fa5649ec41ff78fe1
Lord of Adminster <ttonndda@gmail.com>
2020-11-28 12:38:44 +0100

    Docu edit, hotel validator fix

--- iis_hotel/resources/views/hotels/edit.blade.php
+++ iis_hotel/resources/views/hotels/edit.blade.php
@@ -25,19 +25,6 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="ucet" class="col-4 col-form-label text-right">Account</label>
-
-                            <div class="col-6">
-                                <input id="ucet" type="text" class="form-control @error('ucet') is-invalid @enderror" name="ucet" value="{{ old('ucet', $hotel->ucet) }}" autofocus>
-                                @error('ucet')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
                             <label for="stat" class="col-4 col-form-label text-right">Country</label>
 
                             <div class="col-6">
@@ -101,6 +88,19 @@
 
                                 @error('PSC')
                                     <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="ucet" class="col-4 col-form-label text-right">Account</label>
+
+                            <div class="col-6">
+                                <input id="ucet" type="text" class="form-control @error('ucet') is-invalid @enderror" name="ucet" value="{{ old('ucet', $hotel->ucet) }}" autofocus>
+                                @error('ucet')
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
